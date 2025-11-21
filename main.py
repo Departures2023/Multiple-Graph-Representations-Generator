8c8d0ac15ab7b8438a58469b8bf8c6fd3d06a99d
devanshu <devanshusenp@gmail.com>
2025-11-21 14:34:35 -0600

    placeholder test cases in main file

---
+++ main.py
@@ -0,0 +1,42 @@
+from graph import Graph
+
+if __name__ == "__main__":
+    # Graph 1
+    graph_1_description = [(1, 2), (2, 3), (3, 1)]
+    graph_1_title = "Triangle Graph"
+    graph_1_image = None
+
+    g = Graph(description=graph_1_description, title=graph_1_title, image=graph_1_image)
+    print(g)
+
+    # Graph 2
+    graph_2_description = [(1, 2), (2, 3), (3, 1)]
+    graph_2_title = "C4 Graph"
+    graph_2_image = None
+
+    g = Graph(description=graph_2_description, title=graph_2_title, image=graph_2_image)
+    print(g)
+
+    # Graph 3
+    graph_3_description = [(1, 2), (2, 3), (3, 1)]
+    graph_3_title = "... Graph"
+    graph_3_image = None
+
+    g = Graph(description=graph_3_description, title=graph_3_title, image=graph_3_image)
+    print(g)
+
+    # Graph 4
+    graph_4_description = [(1, 2), (2, 3), (3, 1)]
+    graph_4_title = "... Graph"
+    graph_4_image = None
+
+    g = Graph(description=graph_4_description, title=graph_4_title, image=graph_4_image)
+    print(g)
+
+    # Graph 5
+    graph_5_description = [(1, 2), (2, 3), (3, 1)]
+    graph_5_title = "... Graph"
+    graph_5_image = None
+
+    g = Graph(description=graph_5_description, title=graph_5_title, image=graph_5_image)
+    print(g)
